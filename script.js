80ecd8d0e44df4ca857dad0d7e06abd066c9608c
Chris Kwan <chriswkwan@gmail.com>
2016-02-29 08:37:24 -0500

    Add form for specifying what pokemon number to get

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-var pokemonApiBasePath = "http://pokeapi.co";
+(function () {
+    var pokemonApiBasePath = "http://pokeapi.co";
 
-var getUrl = function (url, callback) {
-    var request = new XMLHttpRequest();
-    request.onload = callback;
-    request.open("GET", url);
-    request.responseType = "json";
-    request.send();
-}
+    var getUrl = function (url, callback) {
+        var request = new XMLHttpRequest();
+        request.onload = callback;
+        request.open("GET", url);
+        request.responseType = "json";
+        request.send();
+    }
 
-var getPokemonSprite = function (id) {
-    // sprites are not in pokemon api v2 yet:
-    // https://github.com/phalt/pokeapi/issues/80
-    var url = pokemonApiBasePath + "/api/v1/pokemon/" + id;
-    getUrl(url, function (e) {
-        var pokemon = e.currentTarget.response;
-        if (pokemon.sprites && pokemon.sprites.length) {
-            var spriteUrl = pokemonApiBasePath + pokemon.sprites[0].resource_uri;
-            getUrl(spriteUrl, function (e) {
-                var sprite = e.currentTarget.response;
-                var pokemonImageElement = document.getElementById("pokemon-image");
-                pokemonImageElement.src = pokemonApiBasePath + sprite.image;
-            });
-        }
-    });
-};
+    var getPokemonSprite = function (id) {
+        // sprites are not in pokemon api v2 yet:
+        // https://github.com/phalt/pokeapi/issues/80
+        var url = pokemonApiBasePath + "/api/v1/pokemon/" + id;
+        getUrl(url, function (e) {
+            var pokemon = e.currentTarget.response;
+            if (pokemon.sprites && pokemon.sprites.length) {
+                var spriteUrl = pokemonApiBasePath + pokemon.sprites[0].resource_uri;
+                getUrl(spriteUrl, function (e) {
+                    var sprite = e.currentTarget.response;
+                    var pokemonImageElement = document.getElementById("pokemon-image");
+                    pokemonImageElement.src = pokemonApiBasePath + sprite.image;
+                });
+            }
+        });
+    };
 
-var getPokemon = function (id) {
-    var url = pokemonApiBasePath + "/api/v2/pokemon/" + id;
-    getUrl(url, function (e) {
-        var pokemon = e.currentTarget.response;
-        var pokemonNameElement = document.getElementById("pokemon-name");
-        pokemonNameElement.innerText = pokemon.name;
+    var getPokemon = function (id) {
+        var url = pokemonApiBasePath + "/api/v2/pokemon/" + id;
+        getUrl(url, function (e) {
+            var pokemon = e.currentTarget.response;
+            var pokemonNameElement = document.getElementById("pokemon-name");
+            pokemonNameElement.innerText = pokemon.name + " #" + pokemon.id;
 
-        getPokemonSprite(id);
-    });
-};
+            getPokemonSprite(id);
+        });
+    };
 
-getPokemon(123);
+    var setupUI = function () {
+        var pokemonNumElement = document.getElementById("pokemon-number");
+        var goBtn = document.getElementById("go-btn");
+
+        goBtn.onclick = function () {
+            getPokemon(pokemonNumElement.value);
+        };
+    };
+
+    setupUI();
+})();
