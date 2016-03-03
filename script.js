873cd6fd4be7fc47c9811975e7a748d6a0447909
Chris Kwan <chriswkwan@gmail.com>
2016-03-03 08:44:28 -0500

    Update javascript

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 (function () {
-    var pokemonApiBasePath = "http://pokeapi.co";
-
     var pokemonNumElement = document.getElementById("pokemon-number");
     var goBtn = document.getElementById("go-btn");
     var randomBtn = document.getElementById("random-btn");
@@ -9,33 +7,11 @@
     var pokemonImageElement = document.getElementById("pokemon-image");
     var pokemonNameElement = document.getElementById("pokemon-name");
 
-    var getUrl = function (url, callback) {
-        var request = new XMLHttpRequest();
-        request.onload = callback;
-        request.open("GET", url);
-        request.responseType = "json";
-        request.send();
-    }
+    var updatePokemonImage = function (pokemonSpriteUrl) {
+        pokemonImageElement.src = pokemonSpriteUrl;
 
-    var getPokemonSprite = function (id) {
-        //cwkTODO this might not support all pokemon that v2 api supports
-
-        // sprites are not in pokemon api v2 yet:
-        // https://github.com/phalt/pokeapi/issues/80
-        var url = pokemonApiBasePath + "/api/v1/pokemon/" + id;
-        getUrl(url, function (e) {
-            var pokemon = e.currentTarget.response;
-            if (pokemon.sprites && pokemon.sprites.length) {
-                var spriteUrl = pokemonApiBasePath + pokemon.sprites[0].resource_uri;
-                getUrl(spriteUrl, function (e) {
-                    var sprite = e.currentTarget.response;
-                    pokemonImageElement.src = pokemonApiBasePath + sprite.image;
-
-                    loadingMessageElement.style.display = "none";
-                    pokemonNameElement.style.display = "block";
-                });
-            }
-        });
+        loadingMessageElement.style.display = "none";
+        pokemonNameElement.style.display = "block";
     };
 
     var capitalize = function (word) {
@@ -60,41 +36,28 @@
         showPokeballAnimation();
     };
 
-    var getPokemon = function (id) {
+    var updatePokemonName = function (pokemon) {
+        pokemonNameElement.innerText = capitalize(pokemon.name) + " #" + pokemon.id;
+    };
+
+    var getPokemonAndUpdateUI = function (number) {
         showLoadingAnimation();
-
-        var url = pokemonApiBasePath + "/api/v2/pokemon/" + id;
-        getUrl(url, function (e) {
-            var pokemon = e.currentTarget.response;
-            pokemonNameElement.innerText = capitalize(pokemon.name) + " #" + pokemon.id;
-
-            getPokemonSprite(id);
-        });
-    };
-
-    var getRandomNumberInRangeInclusive = function (min, max) {
-        // Ref: http://stackoverflow.com/a/7228322
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-
-    var getRandomPokemonNumber = function () {
-        // Pokemon API goes from 1 - 721 (Volcanion)
-        //cwkTODO get this programmatically
-        return getRandomNumberInRangeInclusive(1, 721);
+        randomPokemon.getPokemon(number, updatePokemonName, updatePokemonImage);
     };
 
     var setupUI = function () {
         goBtn.onclick = function () {
-            getPokemon(pokemonNumElement.value);
+            getPokemonAndUpdateUI(pokemonNumElement.value);
         };
 
         randomBtn.onclick = function () {
-            var num = getRandomPokemonNumber();
+            var num = randomPokemon.getRandomPokemonNumber();
             pokemonNumElement.value = num;
-            getPokemon(num);
+            getPokemonAndUpdateUI(num);
         };
     };
 
     setupUI();
-    getPokemon(pokemonNumElement.value);
+
+    getPokemonAndUpdateUI(pokemonNumElement.value);
 })();
