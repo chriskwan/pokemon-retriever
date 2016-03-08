14cc43989b9dd669452be016d2688043a0e9da2d
Chris Kwan <chriswkwan@gmail.com>
2016-03-08 07:27:19 -0500

    Serve pokeball gif locally so we don't have to make a request for it

---
+++ credits.md
@@ -0,0 +1,2 @@
+# Credits
+* Pokeball Animation: http://rockehjamaa.deviantart.com/art/Pokeball-Wub-447084512 (http://orig09.deviantart.net/cd96/f/2014/102/8/c/pokeball_wub_by_rockehjamaa-d7e6km8.gif)
--- script.js
+++ script.js
@@ -23,7 +23,7 @@
 
     var showPokeballAnimation = function () {
         // Source: http://orig09.deviantart.net/cd96/f/2014/102/8/c/pokeball_wub_by_rockehjamaa-d7e6km8.gif
-        pokemonImageElement.src = "http://orig09.deviantart.net/cd96/f/2014/102/8/c/pokeball_wub_by_rockehjamaa-d7e6km8.gif";
+        pokemonImageElement.src = "/pokeball.gif";
     };
 
     var showLoadingTitle = function () {
